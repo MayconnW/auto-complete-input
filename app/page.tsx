97c04f6731a347655d1e13dcf415c43d16f46a21
Mayconn Coelho <mayconnwgo@gmail.com>
2023-05-22 01:45:40 -0300

    feature: consume auto-complete component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+"use client"
 import styles from './page.module.css'
+import { AutoComplete } from './components/auto-complete'
+import {getMatches} from './util/fake-api'
+import { useCallback, useState } from 'react'
 
 export default function Home() {
+  const [itemSelected, setItemSelected] = useState('')
   
+  const onSelectItem = useCallback((item: string) => {
+    setItemSelected(item)
+  }, [])
+
   return (
-    <p>Hello</p>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <AutoComplete fetch={getMatches} onSelectCallback={onSelectItem}/>
+      </div>
+      <div>
+        {itemSelected && <p>Selected Item: {itemSelected}</p>}
+      </div>
+    </div>
   )
 }
